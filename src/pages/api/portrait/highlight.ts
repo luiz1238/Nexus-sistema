24f3a2b7fe1d8438c05629b1c6f6a4dc612a61f1
Morgan Lemos <91709506+luiz1238@users.noreply.github.com>
2026-08-07 16:16:01 -0300

    Update highlight.ts

--- src/pages/api/portrait/highlight.ts
+++ src/pages/api/portrait/highlight.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { broadcast } from '../../../utils/broadcast';
 import { sessionAPI } from '../../../utils/session';
+import prisma from '../../../utils/database';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
@@ -14,7 +15,32 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     return;
   }
 
-  const { playerId, color } = req.body;
+  const { playerId } = req.body;
+  let { color } = req.body;
+
+  // Se a cor não foi passada manualmente, busca a cor do atributo principal do personagem no banco
+  if (playerId && !color) {
+    const playerAttribute = await prisma.playerAttribute.findFirst({
+      where: {
+        player_id: playerId,
+        Attribute: { portrait: 'PRIMARY' },
+      },
+      select: {
+        Attribute: {
+          select: { color: true },
+        },
+      },
+    });
+
+    if (playerAttribute?.Attribute?.color) {
+      color = playerAttribute.Attribute.color;
+    }
+  }
+
+  // Garante a presença do símbolo '#' no código hexadecimal
+  if (color && !color.startsWith('#')) {
+    color = `#${color}`;
+  }
 
   broadcast('portraitHighlight', {
     playerId: playerId ?? null,

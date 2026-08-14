a29727b2aaf686af9b47f5faa1e68688c57214e4
Morgan Lemos <91709506+luiz1238@users.noreply.github.com>
2026-08-13 21:53:50 -0300

    Update info.ts

--- src/pages/api/sheet/player/info.ts
+++ src/pages/api/sheet/player/info.ts
@@ -1,42 +1,58 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import database from '../../../../utils/database';
+import prisma from '../../../../utils/database';
 import { sessionAPI } from '../../../../utils/session';
 import { broadcast } from '../../../../utils/broadcast';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    return handlePost(req, res);
-  }
-  res.status(404).end();
-}
-
-async function handlePost(req: NextApiRequest, res: NextApiResponse) {
-  const player = req.session.player;
-  const npcId: number | undefined = req.body.npcId;
-
-  if (!player || (player.admin && !npcId)) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
     res.status(401).end();
     return;
   }
 
-  const infoID: number | undefined = req.body.id;
-  const value: string | undefined = req.body.value;
-
-  if (!infoID || value === undefined) {
-    res.status(401).send({ message: 'Info ID or value is undefined.' });
+  const player = req.session.player;
+  if (!player) {
+    res.status(401).end();
     return;
   }
 
-  const playerId = npcId ? npcId : player.id;
+  const npcId: number | undefined = req.body.npcId;
+  const targetPlayerId: number | undefined = req.body.playerId;
 
-  await database.playerInfo.update({
+  let playerId = player.id;
+  if (player.admin) {
+    if (npcId) playerId = npcId;
+    else if (targetPlayerId) playerId = targetPlayerId;
+    else {
+      res.status(401).end();
+      return;
+    }
+  }
+
+  const infoID: number | undefined = parseInt(req.body.id);
+  if (!infoID) {
+    res.status(401).send({ message: 'ID da informação está em branco.' });
+    return;
+  }
+
+  const value: string | undefined = req.body.value;
+
+  const info = await prisma.playerInfo.update({
+    where: {
+      player_id_info_id: {
+        player_id: playerId,
+        info_id: infoID,
+      },
+    },
     data: { value },
-    where: { player_id_info_id: { player_id: playerId, info_id: infoID } },
   });
 
   res.end();
 
-  if (!npcId) broadcast('playerInfoChange', { playerId, infoId: infoID, value });
+  broadcast('playerInfoChange', {
+    playerId,
+    infoId: infoID,
+    value: info.value,
+  });
 }
 
 export default sessionAPI(handler);

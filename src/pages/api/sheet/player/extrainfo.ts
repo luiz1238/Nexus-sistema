05a68cbcc3c5314691738759aaf43c52125fc6e8
Morgan Lemos <91709506+luiz1238@users.noreply.github.com>
2026-08-13 21:54:08 -0300

    Update extrainfo.ts

--- src/pages/api/sheet/player/extrainfo.ts
+++ src/pages/api/sheet/player/extrainfo.ts
@@ -1,37 +1,58 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import database from '../../../../utils/database';
+import prisma from '../../../../utils/database';
 import { sessionAPI } from '../../../../utils/session';
+import { broadcast } from '../../../../utils/broadcast';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method === 'POST') return handlePost(req, res);
-	res.status(404).end();
-}
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.status(401).end();
+    return;
+  }
 
-async function handlePost(req: NextApiRequest, res: NextApiResponse) {
-	const player = req.session.player;
-	const npcId: number | undefined = req.body.npcId;
-	
-	if (!player || (player.admin && !npcId)) {
-		res.status(401).end();
-		return;
-	}
-	
-	const id: number | undefined = req.body.id;
-	const value: string | undefined = req.body.value;
+  const player = req.session.player;
+  if (!player) {
+    res.status(401).end();
+    return;
+  }
 
-	if (!id || value === undefined) {
-		res.status(400).send({ message: 'ID ou valor estão em branco.' });
-		return;
-	}
+  const npcId: number | undefined = req.body.npcId;
+  const targetPlayerId: number | undefined = req.body.playerId;
 
-	await database.playerExtraInfo.update({
-		data: { value },
-		where: {
-			player_id_extra_info_id: { player_id: npcId ? npcId : player.id, extra_info_id: id },
-		},
-	});
+  let playerId = player.id;
+  if (player.admin) {
+    if (npcId) playerId = npcId;
+    else if (targetPlayerId) playerId = targetPlayerId;
+    else {
+      res.status(401).end();
+      return;
+    }
+  }
 
-	res.end();
+  const extraInfoID: number | undefined = parseInt(req.body.id);
+  if (!extraInfoID) {
+    res.status(401).send({ message: 'ID da informação extra está em branco.' });
+    return;
+  }
+
+  const value: string | undefined = req.body.value;
+
+  const extraInfo = await prisma.playerExtraInfo.update({
+    where: {
+      player_id_extra_info_id: {
+        player_id: playerId,
+        extra_info_id: extraInfoID,
+      },
+    },
+    data: { value },
+  });
+
+  res.end();
+
+  broadcast('playerExtraInfoChange', {
+    playerId,
+    extraInfoId: extraInfoID,
+    value: extraInfo.value,
+  });
 }
 
 export default sessionAPI(handler);

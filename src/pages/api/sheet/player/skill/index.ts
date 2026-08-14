311c6177696ed42d567f400a8603835beb12379e
Morgan Lemos <91709506+luiz1238@users.noreply.github.com>
2026-08-13 21:53:13 -0300

    Update index.ts

--- src/pages/api/sheet/player/skill/index.ts
+++ src/pages/api/sheet/player/skill/index.ts
@@ -2,100 +2,59 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../../../utils/database';
 import { sessionAPI } from '../../../../../utils/session';
 import { broadcast } from '../../../../../utils/broadcast';
-import type { DiceConfig } from '../../../../../utils/config';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') return handlePost(req, res);
-  if (req.method === 'PUT') return handlePut(req, res);
-  res.status(404).send({ message: 'Supported methods: POST | PUT' });
-}
-
-async function handlePost(req: NextApiRequest, res: NextApiResponse) {
-  const player = req.session.player;
-  const npcId: number | undefined = req.body.npcId;
-
-  if (!player || (player.admin && !npcId)) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
     res.status(401).end();
     return;
   }
 
-  const skillID: number | undefined = req.body.id;
-  const value: number | undefined = req.body.value;
-  const checked: boolean | undefined = req.body.checked;
-  const modifier: number | undefined = req.body.modifier;
-
-  if (!skillID) {
-    res.status(400).send({ message: 'Skill ID or value is undefined.' });
+  const player = req.session.player;
+  if (!player) {
+    res.status(401).end();
     return;
   }
 
-  const playerId = npcId ? npcId : player.id;
+  const npcId: number | undefined = req.body.npcId;
+  const targetPlayerId: number | undefined = req.body.playerId;
+
+  let playerId = player.id;
+  if (player.admin) {
+    if (npcId) playerId = npcId;
+    else if (targetPlayerId) playerId = targetPlayerId;
+    else {
+      res.status(401).end();
+      return;
+    }
+  }
+
+  const skillID: number | undefined = parseInt(req.body.id);
+  if (!skillID) {
+    res.status(401).send({ message: 'ID da perícia está em branco.' });
+    return;
+  }
+
+  const value: number | undefined = req.body.value;
+  const checked: boolean | undefined = req.body.checked;
 
   const skill = await prisma.playerSkill.update({
-    where: { player_id_skill_id: { player_id: playerId, skill_id: skillID } },
-    data: { value, checked, modifier },
+    where: {
+      player_id_skill_id: {
+        player_id: playerId,
+        skill_id: skillID,
+      },
+    },
+    data: { value, checked },
   });
+
+  res.end();
 
   broadcast('playerSkillChange', {
     playerId,
     skillId: skillID,
     value: skill.value,
-    modifier: skill.modifier,
+    checked: skill.checked,
   });
-
-  res.end();
-}
-
-async function handlePut(req: NextApiRequest, res: NextApiResponse) {
-  const player = req.session.player;
-  const npcId: number | undefined = req.body.npcId;
-
-  if (!player || (player.admin && !npcId)) {
-    res.status(401).end();
-    return;
-  }
-
-  const skillID = req.body.id;
-
-  if (!skillID) {
-    res.status(400).send({ message: 'Skill ID is undefined.' });
-    return;
-  }
-
-  const playerId = npcId ? npcId : player.id;
-
-  const [skill, dices] = await prisma.$transaction([
-    prisma.skill.findUnique({
-      where: { id: skillID },
-      select: { startValue: true },
-    }),
-    prisma.config.findUnique({
-      where: { name: 'dice' },
-      select: { value: true },
-    }),
-  ]);
-
-  const playerSkill = await prisma.playerSkill.create({
-    data: {
-      player_id: playerId,
-      skill_id: skillID,
-      value: skill?.startValue || 0,
-      checked: false,
-    },
-    select: {
-      Skill: {
-        select: { id: true, name: true, Specialization: { select: { name: true } } },
-      },
-      value: true,
-      checked: true,
-      modifier: true,
-    },
-  });
-
-  const enableModifiers = ((dices?.value || {}) as DiceConfig)?.skill?.enable_modifiers;
-  if (!enableModifiers) playerSkill.modifier = null as any;
-
-  res.send({ skill: playerSkill });
 }
 
 export default sessionAPI(handler);

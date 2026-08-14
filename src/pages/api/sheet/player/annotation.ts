d94f398aa721a01ec56a3518b7b92dd13dc2a1a8
Morgan Lemos <91709506+luiz1238@users.noreply.github.com>
2026-08-13 21:54:46 -0300

    Update annotation.ts

--- src/pages/api/sheet/player/annotation.ts
+++ src/pages/api/sheet/player/annotation.ts
@@ -1,34 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import database from '../../../../utils/database';
+import prisma from '../../../../utils/database';
 import { sessionAPI } from '../../../../utils/session';
+import { broadcast } from '../../../../utils/broadcast';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method === 'POST') return handlePost(req, res);
-	res.status(404).end();
-}
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.status(401).end();
+    return;
+  }
 
-async function handlePost(req: NextApiRequest, res: NextApiResponse) {
-	const player = req.session.player;
+  const player = req.session.player;
+  if (!player) {
+    res.status(401).end();
+    return;
+  }
 
-	if (!player) {
-		res.status(401).end();
-		return;
-	}
+  const npcId: number | undefined = req.body.npcId;
+  const targetPlayerId: number | undefined = req.body.playerId;
 
-	const value: string | undefined = req.body.value;
-	const npcId: number | undefined = req.body.npcId;
+  let playerId = player.id;
+  if (player.admin) {
+    if (npcId) playerId = npcId;
+    else if (targetPlayerId) playerId = targetPlayerId;
+    else {
+      res.status(401).end();
+      return;
+    }
+  }
 
-	if (value === undefined) {
-		res.status(400).send({ message: 'Valor da anotação está em branco.' });
-		return;
-	}
+  const value: string | undefined = req.body.value;
 
-	await database.playerNote.update({
-		data: { value },
-		where: { player_id: npcId ? npcId : player.id },
-	});
+  await prisma.player.update({
+    where: { id: playerId },
+    data: { annotation: value },
+  });
 
-	res.end();
+  res.end();
+
+  broadcast('playerAnnotationChange', {
+    playerId,
+    value,
+  });
 }
 
 export default sessionAPI(handler);

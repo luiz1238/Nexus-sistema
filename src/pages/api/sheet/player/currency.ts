2573e2c6c87bf7726594c95fc72b1b726d30455d
Morgan Lemos <91709506+luiz1238@users.noreply.github.com>
2026-08-13 21:54:22 -0300

    Update currency.ts

--- src/pages/api/sheet/player/currency.ts
+++ src/pages/api/sheet/player/currency.ts
@@ -1,42 +1,58 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import database from '../../../../utils/database';
+import prisma from '../../../../utils/database';
 import { sessionAPI } from '../../../../utils/session';
 import { broadcast } from '../../../../utils/broadcast';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    return handlePost(req, res);
-  }
-  res.status(404).end();
-}
-
-async function handlePost(req: NextApiRequest, res: NextApiResponse) {
-  const player = req.session.player;
-  const npcId: number | undefined = req.body.npcId;
-
-  if (!player || (player.admin && !npcId)) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
     res.status(401).end();
     return;
   }
 
-  const currencyID: number | undefined = req.body.id;
-  const value: string | undefined = req.body.value;
-
-  if (!currencyID || value === undefined) {
-    res.status(401).send({ message: 'Currency ID or value is undefined.' });
+  const player = req.session.player;
+  if (!player) {
+    res.status(401).end();
     return;
   }
 
-  const playerId = npcId ? npcId : player.id;
+  const npcId: number | undefined = req.body.npcId;
+  const targetPlayerId: number | undefined = req.body.playerId;
 
-  await database.playerCurrency.update({
+  let playerId = player.id;
+  if (player.admin) {
+    if (npcId) playerId = npcId;
+    else if (targetPlayerId) playerId = targetPlayerId;
+    else {
+      res.status(401).end();
+      return;
+    }
+  }
+
+  const currencyID: number | undefined = parseInt(req.body.id);
+  if (!currencyID) {
+    res.status(401).send({ message: 'ID da moeda está em branco.' });
+    return;
+  }
+
+  const value: string | undefined = req.body.value;
+
+  const currency = await prisma.playerCurrency.update({
+    where: {
+      player_id_currency_id: {
+        player_id: playerId,
+        currency_id: currencyID,
+      },
+    },
     data: { value },
-    where: { player_id_currency_id: { player_id: playerId, currency_id: currencyID } },
   });
 
   res.end();
 
-  broadcast('playerCurrencyChange', { playerId, currencyId: currencyID, value });
+  broadcast('playerCurrencyChange', {
+    playerId,
+    currencyId: currencyID,
+    value: currency.value,
+  });
 }
 
 export default sessionAPI(handler);
